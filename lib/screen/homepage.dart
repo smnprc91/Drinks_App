8e8b6b996eb9392a99741eb25cbd6415eb4080f0
smnprc91 <workshopt.arealavoro@gmail.com>
2021-09-06 14:13:06 +0200

    try json in locale

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,18 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List _items = [];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/test.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["items"];
+    });
+  }
+
+  @override
+  void initState() {
+    readJson();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('ciaone rubicone'),
-      
+    return Scaffold(
+      body: Column(
+        children: items(),
+      ),
     );
   }
+}
+
+items() {
+  return Container(color: Colors.amber);
 }
