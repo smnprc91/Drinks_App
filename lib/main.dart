0afb50172a222b27f1de0cc04b507b2e45600fa8
yescit <github@yesc.it>
2021-12-04 14:10:31 +0100

    prova da mac3

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 int? isviewed;
 void main() async {
-  //CIAONE
+  //CIAONE 2334
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   isviewed = prefs.getInt('onBoard');
