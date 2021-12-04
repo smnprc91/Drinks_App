2afbc86b71f68e25c2aee5f9e1e6b72848767b4b
yescit <github@yesc.it>
2021-12-04 13:04:31 +0100

    prova da mac

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 int? isviewed;
 void main() async {
-   WidgetsFlutterBinding.ensureInitialized();
+  //CIAONE CIAONE
+  WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   isviewed = prefs.getInt('onBoard');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
       home: isviewed != 0 ? FirstStartingPage() : HomePage(),
     );
   }
 }
-
-
-
